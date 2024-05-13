5a6154567684f55f2760897eae8b774b8397f896
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-05-13 16:40:32 +0200

    Patch "InRaid" for better mod support

---
+++ Fika.Core/Coop/Patches/AbstractGame/AbstractGame_InRaid_Patch.cs
@@ -0,0 +1,25 @@
+﻿using Aki.Reflection.Patching;
+using EFT;
+using Fika.Core.Coop.GameMode;
+using System.Reflection;
+
+namespace Fika.Core.Coop.Patches
+{
+    /// <summary>
+    /// Used to support mods that rely on the InRaid method, which normally casts to <see cref="EFT.LocalGame"/>
+    /// </summary>
+    internal class AbstractGame_InRaid_Patch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(AbstractGame).GetProperty(nameof(AbstractGame.InRaid)).GetGetMethod();
+        }
+
+        [PatchPrefix]
+        private static bool PreFix(AbstractGame __instance, bool __result)
+        {
+            __result = __instance is CoopGame;
+            return false;
+        }
+    }
+}
