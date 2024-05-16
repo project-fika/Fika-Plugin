53902b0a2e91bfe156b0ed31c0221a681bfadac6
CWX <cwxdev@outlook.com>
2024-05-16 18:55:12 +0100

    Fix name formatting for bots

--- Fika.Core/Coop/Components/CoopHandler.cs
+++ Fika.Core/Coop/Components/CoopHandler.cs
@@ -450,7 +450,7 @@ namespace Fika.Core.Coop.Components
                 position,
                 Quaternion.identity,
                 "Player",
-                isAI == true ? $"Bot_{netId}" : $"Player_{profile.Nickname}_",
+                isAI == true ? $"Bot_{netId}_" : $"Player_{profile.Nickname}_",
                 EPointOfView.ThirdPerson,
                 profile,
                 isAI,
