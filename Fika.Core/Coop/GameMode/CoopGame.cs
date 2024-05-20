1ddec4b62511b04dc2b2b64bdb3ac3c5b77e3e0e
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-05-20 23:24:59 +0200

    Fixes #53 - Remove dog tag on exfil

--- Fika.Core/Coop/GameMode/CoopGame.cs
+++ Fika.Core/Coop/GameMode/CoopGame.cs
@@ -1524,6 +1524,18 @@ namespace Fika.Core.Coop.GameMode
             CoopPlayer myPlayer = (CoopPlayer)Singleton<GameWorld>.Instance.MainPlayer;
             myPlayer.PacketSender?.DestroyThis();
 
+            if (myPlayer.Side != EPlayerSide.Savage)
+            {
+                if (myPlayer.Equipment.GetSlot(EquipmentSlot.Dogtag).ContainedItem != null)
+                {
+                    GStruct414<GClass2785> result = InteractionsHandlerClass.Remove(myPlayer.Equipment.GetSlot(EquipmentSlot.Dogtag).ContainedItem, myPlayer.GClass2761_0, false, true);
+                    if (result.Error != null)
+                    {
+                        FikaPlugin.Instance.FikaLogger.LogWarning("CoopGame::Stop: Error removing dog tag!");
+                    }
+                }
+            }
+
             if (!myPlayer.ActiveHealthController.IsAlive && exitStatus == ExitStatus.Survived)
             {
                 exitStatus = ExitStatus.Killed;
