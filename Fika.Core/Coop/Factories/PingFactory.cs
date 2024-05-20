f6743559667cb1a9cba531dea3b2832d72d7c669
Shynd <Outshynd@users.noreply.github.com>
2024-05-20 01:38:51 -0400

    initial testing fix for pings in scopes

--- Fika.Core/Coop/Factories/PingFactory.cs
+++ Fika.Core/Coop/Factories/PingFactory.cs
@@ -1,5 +1,7 @@
 ﻿using Comfort.Common;
 using EFT;
+using EFT.CameraControl;
+using EFT.InventoryLogic;
 using Fika.Core.Bundles;
 using Fika.Core.Coop.Players;
 using UnityEngine;
@@ -76,6 +78,52 @@ public static class PingFactory
             }
 
             Camera camera = CameraClass.Instance.Camera;
+            Vector3 screenPoint = Vector3.zero;
+
+            if (mainPlayer.ProceduralWeaponAnimation.IsAiming && mainPlayer.ProceduralWeaponAnimation.CurrentScope.IsOptic)
+            {
+                //SightComponent currentSightComponent = mainPlayer.ProceduralWeaponAnimation.CurrentScope.Mod;
+                SightComponent currentSightComponent = mainPlayer.ProceduralWeaponAnimation.CurrentAimingMod;
+
+                if (currentSightComponent != null)
+                {
+                    float opticZoomLevel = currentSightComponent.GetCurrentOpticZoom();
+
+                    //only calculate optic screen point if zoom is higher than 1
+                    if (opticZoomLevel > 1.0f)
+                    {
+                        Camera opticCamera = CameraClass.Instance.OpticCameraManager.Camera;
+
+                        if (opticCamera != null)
+                        {
+                            //calculate where the optic is relative to the center of the screen due to weapon sway
+                            OpticSight currentOptic = mainPlayer.ProceduralWeaponAnimation.HandsContainer.Weapon.GetComponentInChildren<OpticSight>();
+
+                            if (currentOptic != null)
+                            {
+                                Vector3 opticCenterScreenPosition = camera.WorldToScreenPoint(currentOptic.LensRenderer.transform.position);
+                                Vector3 opticCenterScreenOffset = opticCenterScreenPosition - (new Vector3(Screen.width, Screen.height, 0) / 2);
+
+                                //TO-DO?: calculate optic radius & don't use optic camera to render if distance from middle of screen is > radius
+
+                                //calculate where the zoomed in camera & world camera correspond to each other
+                                float opticScale = Screen.height / opticCamera.scaledPixelHeight;
+                                Vector3 opticCameraOffset = new Vector3(x: (camera.pixelWidth / 2 - opticCamera.pixelWidth / 2), y: (camera.pixelHeight / 2 - opticCamera.pixelHeight / 2), z: 0);
+                                Vector3 scopeScreenPoint = (opticCamera.WorldToScreenPoint(hitPoint) + opticCameraOffset) * opticScale;
+
+                                screenPoint = scopeScreenPoint + opticCenterScreenOffset;
+                                //screenPoint = scopeScreenPoint;
+                            }
+                        }
+                    }
+                }
+            }
+
+            //no optic, not aiming, or optic is not zoomed (thus no pip camera component)
+            if (screenPoint ==  Vector3.zero)
+            {
+                screenPoint = camera.WorldToScreenPoint(hitPoint);
+            }
 
             if (CameraClass.Instance.SSAA != null && CameraClass.Instance.SSAA.isActiveAndEnabled)
             {
@@ -84,10 +132,9 @@ public static class PingFactory
                 screenScale = outputWidth / inputWidth;
             }
 
-            Vector3 screenPoint = camera.WorldToScreenPoint(hitPoint);
             if (screenPoint.z > 0)
             {
-                float distanceToCenter = Vector3.Distance(screenPoint, new Vector3(x: Screen.width, Screen.height, 0) / 2);
+                float distanceToCenter = Vector3.Distance(screenPoint, new Vector3(x: Screen.width, y: Screen.height, 0) / 2);
 
                 if (distanceToCenter < 200)
                 {
