3a0d0fd08a37bbe0e872eba3c173f7c462a49d44
Archangel <jesse@archangel.wtf>
2024-05-22 19:10:58 +0200

    Do not disable OfflineSaveProfilePatch by default

--- Fika.Core/FikaPlugin.cs
+++ Fika.Core/FikaPlugin.cs
@@ -460,7 +460,6 @@ namespace Fika.Core
             new BTRInteractionPatch().Disable();
             new BTRExtractPassengersPatch().Disable();
             new BTRPatch().Disable();
-            new OfflineSaveProfilePatch().Disable(); //Disable this as we've moved it forward immediately after extraction or death
         }
 
         private void EnableOverridePatches()
