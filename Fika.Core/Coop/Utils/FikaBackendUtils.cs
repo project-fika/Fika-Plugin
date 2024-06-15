20eb223b962e407c0e69c96bb7998857cc755e34
yhl452493373 <yhl452493373@gmail.com>
2024-06-16 01:51:06 +0800

    remove lowercase letters

--- Fika.Core/Coop/Utils/FikaBackendUtils.cs
+++ Fika.Core/Coop/Utils/FikaBackendUtils.cs
@@ -93,10 +93,9 @@ namespace Fika.Core.Coop.Utils
         private static string GenerateRaidCode(int length)
         {
             var random = new Random();
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length)
-                .Select(s => s[random.Next(s.Length)]).ToArray())
-                .ToUpper();
+                .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
