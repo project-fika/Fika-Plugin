45355065863d5d82cdc03f4e1fa42922abf5841b
hel0t <sow-helot.0r@icloud.com>
2024-06-20 18:58:57 +1000

    Add group utils helpers

--- Fika.Core/Coop/Utils/FikaGroupUtils.cs
+++ Fika.Core/Coop/Utils/FikaGroupUtils.cs
@@ -2,5 +2,19 @@ namespace Fika.Core.Coop.Utils;
 
 public static class FikaGroupUtils
 {
-    public static MatchmakerPlayerControllerClass GroupController { get; set; } 
+    public static MatchmakerPlayerControllerClass GroupController { get; set; }
+    
+    public static bool InGroup => GroupController is { GroupPlayers.Count: >= 1 };
+    public static int GroupSize => InGroup ? GroupController.GroupPlayers.Count : 1;
+    public static bool IsGroupLeader
+    {
+        get
+        {
+            if (GroupController?.Group == null) return true;
+            if (GroupController?.GroupPlayers == null) return true;
+            if (GroupController.GroupPlayers.Count <= 1) return true;
+            if (GroupController?.Group?.Owner?.Value?.Id == null) return true;
+            return GroupController.Group.Owner.Value.Id == FikaBackendUtils.Profile.ProfileId || GroupController.IsLeader;
+        }
+    }
 }
