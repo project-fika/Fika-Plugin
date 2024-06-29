0d8b1dd16fb49a945591763124baae583563c7b5
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-06-29 09:56:23 +0200

    Update to latest SPT mapper

--- Fika.Core/Coop/ClientClasses/CoopClientInventoryController.cs
+++ Fika.Core/Coop/ClientClasses/CoopClientInventoryController.cs
@@ -16,10 +16,7 @@ namespace Fika.Core.Coop.ClientClasses
     public sealed class CoopClientInventoryController(Player player, Profile profile, bool examined) : Player.PlayerOwnerInventoryController(player, profile, examined)
     {
         public override bool HasDiscardLimits => false;
-
-
         ManualLogSource BepInLogger { get; set; } = BepInEx.Logging.Logger.CreateLogSource(nameof(CoopClientInventoryController));
-
         private readonly Player Player = player;
         private CoopPlayer CoopPlayer => (CoopPlayer)Player;
 
