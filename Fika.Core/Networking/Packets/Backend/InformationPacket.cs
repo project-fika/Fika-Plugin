370b9a12f6df27c53ab475e950ffb422023fbe57
hel0t <sow-helot.0r@icloud.com>
2024-06-30 19:26:56 +0930

    update info packet to include group info

--- Fika.Core/Networking/Packets/Backend/InformationPacket.cs
+++ Fika.Core/Networking/Packets/Backend/InformationPacket.cs
@@ -7,23 +7,23 @@ namespace Fika.Core.Networking
     public struct InformationPacket(bool isRequest) : INetSerializable
     {
         public bool IsRequest = isRequest;
-        public int NumberOfPlayers = 0;
-        public int ReadyPlayers = 0;
+        public int Connected = 0;
+        public int Ready = 0;
         public string GroupId = "";
         
         public void Deserialize(NetDataReader reader)
         {
             IsRequest = reader.GetBool();
-            NumberOfPlayers = reader.GetInt();
-            ReadyPlayers = reader.GetInt();
+            Connected = reader.GetInt();
+            Ready = reader.GetInt();
             GroupId = reader.GetString();
         }
 
         public void Serialize(NetDataWriter writer)
         {
             writer.Put(IsRequest);
-            writer.Put(NumberOfPlayers);
-            writer.Put(ReadyPlayers);
+            writer.Put(Connected);
+            writer.Put(Ready);
             writer.Put(GroupId);
         }
     }
