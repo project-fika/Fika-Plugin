84ba43dc7fe54230a0f37321efdad2d9a4ec7b13
hel0t <sow-helot.0r@icloud.com>
2024-06-30 19:29:28 +0930

    add join group raid notification handler

---
+++ Fika.Core/Coop/Patches/MatchmakerPlayerController_Patch.cs
@@ -0,0 +1,37 @@
+using SPT.Reflection.Patching;
+using System.Reflection;
+using Fika.Core.Coop.Utils;
+using Fika.Core.Networking.Http;
+
+namespace Fika.Core.Coop.Patches
+{
+    public class MatchmakerPlayerController_Patch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod() => typeof(MatchmakerPlayerControllerClass).GetMethod("NotificationReceivedHandler");
+
+        [PatchPrefix]
+        private static bool PatchPrefix(MatchmakerPlayerControllerClass __instance, NotificationClass notification)
+        {
+            return notification switch
+            {
+                GClass2020 _ => false,
+                _ => true
+            };
+        }
+
+        [PatchPostfix]
+        private static void PostPrefix(MatchmakerPlayerControllerClass __instance, NotificationClass notification)
+        {
+            if (notification is not GClass2020) return;
+            
+            // Get the serverId of the match the group leader is joining
+            var raidGroup = FikaRequestHandler.GetGroupRaid();
+            
+            // Do nothing if the group leader isn't in a raid
+            if (raidGroup.ServerId is not { Length: > 0 }) return;
+            
+            // Join the raid
+            FikaBackendUtils.MatchMakerUIScript.JoinMatchCoroutine(FikaBackendUtils.Profile.ProfileId, raidGroup.ServerId);
+        }
+    }
+}
