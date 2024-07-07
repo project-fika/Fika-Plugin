479fe6801b6b028f7bc3cec1f4617bbe96987903
Archangel <jesse@archangel.wtf>
2024-07-07 18:22:26 +0200

    Re-add this comment on the client

--- Fika.Core/Coop/BTR/FikaBTRManager_Client.cs
+++ Fika.Core/Coop/BTR/FikaBTRManager_Client.cs
@@ -243,7 +243,7 @@ namespace Fika.Core.Coop.BTR
             var botsController = Singleton<IBotGame>.Instance.BotsController;
             btrBotService = botsController.BotTradersServices.BTRServices;
             btrController.method_3(); // spawns server-side BTR game object
-            botsController.BotSpawner.SpawnBotBTR(); // spawns the scav bot which controls the BTR's turret
+            //botsController.BotSpawner.SpawnBotBTR(); // spawns the scav bot which controls the BTR's turret
 
             // Initial BTR configuration
             btrServerSide = btrController.BtrVehicle;
