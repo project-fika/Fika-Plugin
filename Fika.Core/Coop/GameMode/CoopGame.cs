644ce3f4042dc1edf3200a4acadd59b8680ee0b2
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-07-18 16:46:59 +0200

    Fix build error

--- Fika.Core/Coop/GameMode/CoopGame.cs
+++ Fika.Core/Coop/GameMode/CoopGame.cs
@@ -43,7 +43,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Reflection;
 using System.Threading.Tasks;
 using UnityEngine;
