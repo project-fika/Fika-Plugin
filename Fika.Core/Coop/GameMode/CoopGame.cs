c8f5a311a527a6a7434b58c2f6662729561d5535
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-07-19 20:12:10 +0200

    Smh

--- Fika.Core/Coop/GameMode/CoopGame.cs
+++ Fika.Core/Coop/GameMode/CoopGame.cs
@@ -28,6 +28,7 @@ using Fika.Core.Modding.Events;
 using Fika.Core.Networking;
 using Fika.Core.Networking.Http;
 using Fika.Core.Networking.Http.Models;
+using Fika.Core.Networking.Packets;
 using Fika.Core.Networking.Packets.Communication;
 using Fika.Core.Networking.Packets.GameWorld;
 using Fika.Core.UI.Models;
