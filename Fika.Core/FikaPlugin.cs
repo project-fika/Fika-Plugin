ecba49eb3cdaa2e161cc136b5911551d0599b05c
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-07-23 16:02:04 +0200

    Fix namespace

--- Fika.Core/FikaPlugin.cs
+++ Fika.Core/FikaPlugin.cs
@@ -14,7 +14,6 @@ using Fika.Core.Coop.World;
 using Fika.Core.EssentialPatches;
 using Fika.Core.Models;
 using Fika.Core.Networking.Http;
-using Fika.Core.SPTSupport.Scav;
 using Fika.Core.UI;
 using Fika.Core.UI.Models;
 using Fika.Core.UI.Patches;
