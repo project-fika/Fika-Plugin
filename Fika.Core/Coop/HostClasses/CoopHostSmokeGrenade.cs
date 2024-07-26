d92811ee5393ce00287f0778d5a361e610049e57
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-07-26 08:54:18 +0200

    New grenade sync system

---
+++ Fika.Core/Coop/HostClasses/CoopHostSmokeGrenade.cs
@@ -0,0 +1,7 @@
+﻿namespace Fika.Core.Coop.HostClasses
+{
+    public class CoopHostSmokeGrenade : SmokeGrenade
+    {
+        public override bool HasNetData => true;
+    }
+}
---
+++ Fika.Core/Coop/HostClasses/HostGrenadeFactory.cs
@@ -0,0 +1,13 @@
+﻿using EFT;
+using UnityEngine;
+
+namespace Fika.Core.Coop.HostClasses
+{
+    public class HostGrenadeFactory : GClass676
+    {
+        public override Grenade AddGrenade(GameObject gameObject)
+        {
+            return gameObject.AddComponent<CoopHostGrenade>();
+        }
+    }
+}
