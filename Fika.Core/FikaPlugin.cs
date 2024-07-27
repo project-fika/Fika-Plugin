2aa743febfc0d9dacdf495040776aeb0d310b427
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-07-27 18:27:09 +0200

    Update ver

--- Fika.Core/FikaPlugin.cs
+++ Fika.Core/FikaPlugin.cs
@@ -45,7 +45,7 @@ namespace Fika.Core
     /// Originally by: Paulov <br/>
     /// Re-written by: Lacyway
     /// </summary>
-    [BepInPlugin("com.fika.core", "Fika.Core", "0.9.8964")]
+    [BepInPlugin("com.fika.core", "Fika.Core", "0.9.8974")]
     [BepInProcess("EscapeFromTarkov.exe")]
     [BepInDependency("com.SPT.custom", BepInDependency.DependencyFlags.HardDependency)] // This is used so that we guarantee to load after spt-custom, that way we can disable its patches
     [BepInDependency("com.SPT.singleplayer", BepInDependency.DependencyFlags.HardDependency)] // This is used so that we guarantee to load after spt-singleplayer, that way we can disable its patches
