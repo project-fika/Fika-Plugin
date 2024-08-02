99972187184245bde59e21538a3b8f8c70216e59
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-08-02 21:45:48 +0200

    Update build script

--- Properties/build/goldmaster-core.bat
+++ Properties/build/goldmaster-core.bat
@@ -1,7 +1,8 @@
 @echo off
 
 : call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+: call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
 
 cd ../Build/BepInEx/plugins
 sn -Ra Fika.Core.dll ../../../Properties/signing/private.snk
