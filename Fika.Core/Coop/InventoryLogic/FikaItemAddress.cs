14f3fae07ecc6253a30fb7f28fba9eb66729f2f6
Archangel <jesse@archangel.wtf>
2024-08-21 12:08:52 +0200

    Add FikaitemAddress

---
+++ Fika.Core/Coop/InventoryLogic/FikaItemAddress.cs
@@ -0,0 +1,19 @@
+﻿using EFT.InventoryLogic;
+
+namespace Fika.Core.Coop.InventoryLogic
+{
+    public class FikaItemAddress : GClass3010
+    {
+        public Item Item
+        {
+            get
+            {
+                return Grid.GetItemAt(LocationInGrid);
+            }
+        }
+
+        public FikaItemAddress(StashGridClass grid, LocationInGrid locationInGrid) : base(grid, locationInGrid)
+        {
+        }
+    }
+}
