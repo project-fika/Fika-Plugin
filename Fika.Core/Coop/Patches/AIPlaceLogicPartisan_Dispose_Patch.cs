491ac96dce158edf09de7d71a7f32886c2222ff8
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-09-24 15:35:20 +0200

    Update documentation

--- Fika.Core/Coop/Patches/AIPlaceLogicPartisan_Dispose_Patch.cs
+++ Fika.Core/Coop/Patches/AIPlaceLogicPartisan_Dispose_Patch.cs
@@ -1,8 +1,12 @@
 ﻿using SPT.Reflection.Patching;
+using System;
 using System.Reflection;
 
 namespace Fika.Core.Coop.Patches
 {
+	/// <summary>
+	/// Fixes a live bug where there is no null check on the <see cref="AIPlaceInfo"/> during <see cref="AIPlaceLogicPartisan.Dispose"/>, causing a<see cref="NullReferenceException"/> if it is null
+	/// </summary>
 	public class AIPlaceLogicPartisan_Dispose_Patch : ModulePatch
 	{
 		protected override MethodBase GetTargetMethod()
