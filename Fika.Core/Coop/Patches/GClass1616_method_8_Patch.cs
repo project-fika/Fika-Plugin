676d6aaaad2746b71a57fb1725737205af30e34a
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-10-08 12:28:35 +0200

    Add patch to get trader service for clients

---
+++ Fika.Core/Coop/Patches/GClass1616_method_8_Patch.cs
@@ -0,0 +1,25 @@
+﻿using EFT;
+using Fika.Core.Coop.Players;
+using Fika.Core.Coop.Utils;
+using SPT.Reflection.Patching;
+using System.Reflection;
+
+namespace Fika.Core.Coop.Patches
+{
+	public class GClass1616_method_8_Patch : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(GClass1616).GetMethod(nameof(GClass1616.method_8));
+		}
+
+		[PatchPrefix]
+		public static void Prefix(Player player)
+		{
+			if (FikaBackendUtils.IsClient && player.IsYourPlayer && player is CoopPlayer coopPlayer)
+			{
+				coopPlayer.InventoryController.GetTraderServicesDataFromServer(FikaGlobals.TransitTraderId);
+			}
+		}
+	}
+}
