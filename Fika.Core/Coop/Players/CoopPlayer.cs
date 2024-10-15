111af757a8a906462288b64e7c13ea41a499c51a
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-10-15 23:16:41 +0200

    Remove import

--- Fika.Core/Coop/Players/CoopPlayer.cs
+++ Fika.Core/Coop/Players/CoopPlayer.cs
@@ -24,7 +24,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Threading.Tasks;
 using UnityEngine;
 using static Fika.Core.Coop.ClientClasses.CoopClientInventoryController;
