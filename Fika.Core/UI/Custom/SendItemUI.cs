88741f7447a02f05a68ab18c5bb368af216216de
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-10-21 12:05:27 +0200

    Update bundles and make send item menu draggable

--- Fika.Core/UI/Custom/SendItemUI.cs
+++ Fika.Core/UI/Custom/SendItemUI.cs
@@ -1,4 +1,5 @@
-﻿using Fika.Core.Utils;
+﻿using EFT.UI;
+using Fika.Core.Utils;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -22,5 +23,8 @@ public class SendItemUI : MonoBehaviour
 	{
 		HeaderText.text = LocaleUtils.UI_SENDITEM_HEADER.Localized();
 		SendText.text = LocaleUtils.UI_SENDITEM_BUTTON.Localized();
+		GameObject gameObjectToAdd = gameObject.transform.GetChild(0).GetChild(0).gameObject;
+		RectTransform rectTransform = gameObjectToAdd.RectTransform();
+		gameObjectToAdd.AddComponent<UIDragComponent>().Init(rectTransform, true);
 	}
 }
