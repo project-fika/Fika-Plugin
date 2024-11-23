1e1f33b378b0e9f32ddf8a85da019739d6168a63
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-11-23 12:14:59 +0100

    Add botreload patch

--- Fika.Core/FikaPlugin.cs
+++ Fika.Core/FikaPlugin.cs
@@ -337,6 +337,7 @@ namespace Fika.Core
 			new GameWorld_ThrowItem_Patch().Enable();
 			new RaidSettingsWindow_Show_Patch().Enable();
 			new TransitControllerAbstractClass_Exist_Patch().Enable();
+			new BotReload_method_1_Patch().Enable();
 
 #if DEBUG
 			TasksExtensions_HandleFinishedTask_Patches.Enable();
