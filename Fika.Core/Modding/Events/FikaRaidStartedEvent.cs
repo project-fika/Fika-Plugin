82b402cdc983aa0ea2aad3342f9acca8d5aa96fd
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-12-01 17:10:37 +0100

    Add new event for when the raid starts

---
+++ Fika.Core/Modding/Events/FikaRaidStartedEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace Fika.Core.Modding.Events
+{
+	public class FikaRaidStartedEvent : FikaEvent
+	{
+		public bool IsServer;
+
+		internal FikaRaidStartedEvent(bool isServer)
+		{
+			IsServer = isServer;
+		}
+	}
+}
