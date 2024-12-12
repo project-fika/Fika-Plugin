8ffa9c89c03efa49b70108f2aeac9417effc580c
Archangel <jesse@archangel.wtf>
2024-12-12 23:46:26 +0100

    Disable server scenes as client

---
+++ Fika.Core/Coop/Patches/GClass2013_method_0_Patch.cs
@@ -0,0 +1,24 @@
+﻿using Fika.Core.Coop.Utils;
+using SPT.Reflection.Patching;
+using System.Reflection;
+
+namespace Fika.Core.Coop.Patches
+{
+	internal class GClass2013_method_0_Patch : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(GClass2013).GetMethod(nameof(GClass2013.method_0));
+		}
+
+		[PatchPrefix]
+		public static void Prefix(ref GStruct238 preset)
+		{
+			if (FikaBackendUtils.IsClient)
+			{
+				Logger.LogInfo("Disabling server scenes");
+				preset.DisableServerScenes = true;
+			}
+		}
+	}
+}
