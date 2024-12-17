42611ae153b0dc43dcb689f0cdc571f2121dab52
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-12-17 21:19:24 +0100

    Remove import

--- Fika.Core/FikaPlugin.cs
+++ Fika.Core/FikaPlugin.cs
@@ -29,7 +29,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
