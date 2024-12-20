534db8b26c90eb1967364e425f7b59b5d78df91b
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-12-20 08:28:40 +0100

    Fix transpilers

---
+++ Fika.Core/Coop/Patches/PlayerPatches/Player_ManageAggressor_Patch.cs
@@ -0,0 +1,38 @@
+﻿using EFT;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Fika.Core.Coop.Patches
+{
+	/// <summary>
+	/// This patch stops BSGs dogtag handling as it is poorly executed
+	/// </summary>
+	public class Player_ManageAggressor_Patch : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			//Check for gclass increments
+			return typeof(LocalPlayer).GetMethod(nameof(LocalPlayer.ManageAggressor));
+		}
+
+		[PatchTranspiler]
+		public static IEnumerable<CodeInstruction> Transpile(IEnumerable<CodeInstruction> instructions)
+		{
+			// Create a new set of instructions
+			List<CodeInstruction> instructionsList =
+			[
+				new CodeInstruction(OpCodes.Ldarg_0),
+				new CodeInstruction(OpCodes.Ldarg_1),
+				new CodeInstruction(OpCodes.Ldarg_2),
+				new CodeInstruction(OpCodes.Ldarg_3),
+				new CodeInstruction(OpCodes.Call, typeof(Player).GetMethod(nameof(Player.ManageAggressor))),
+				new CodeInstruction(OpCodes.Ret)
+			];
+
+			return instructionsList;
+		}
+	}
+}
