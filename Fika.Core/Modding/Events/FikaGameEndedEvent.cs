05db3d28ee4350e1d3609336ad8fd09aa8d3d348
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-12-30 20:00:12 +0100

    Add new events - Game ended - Peer connected

---
+++ Fika.Core/Modding/Events/FikaGameEndedEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace Fika.Core.Modding.Events
+{
+	public class FikaGameEndedEvent : FikaEvent
+	{
+		public bool IsServer { get; }
+
+		internal FikaGameEndedEvent(bool isServer)
+		{
+			IsServer = isServer;
+		}
+	}
+}
