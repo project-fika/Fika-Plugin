fd77a537fd9def88bf375b9861aff678897d2fc6
Archangel <jesse@archangel.wtf>
2025-01-07 15:18:53 +0100

    ﻿Update patch

--- Fika.Core/UI/Patches/TarkovApplication_method_18_Patch.cs
+++ Fika.Core/UI/Patches/TarkovApplication_method_18_Patch.cs
@@ -7,23 +7,23 @@ using System.Reflection;
 
 namespace Fika.Core.UI.Patches
 {
-    /// <summary>
-    /// The intention of this patch is to enable FikaNotificationManager after NotificationManagerClass and the NotifierView are initialized.
-    /// </summary>
-    public class TarkovApplication_method_18_Patch : ModulePatch
-    {
-        protected override MethodBase GetTargetMethod()
-        {
-            return typeof(TarkovApplication).GetMethod(nameof(TarkovApplication.method_18));
-        }
+	/// <summary>
+	/// The intention of this patch is to enable FikaNotificationManager after NotificationManagerClass and the NotifierView are initialized.
+	/// </summary>
+	public class TarkovApplication_method_18_Patch : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(TarkovApplication).GetMethod(nameof(TarkovApplication.method_16));
+		}
 
-        [PatchPostfix]
-        internal static void Postfix()
-        {
-            if (!FikaNotificationManager.Exists)
-            {
-                Singleton<PreloaderUI>.Instance.gameObject.AddComponent<FikaNotificationManager>();
-            }
-        }
-    }
+		[PatchPostfix]
+		internal static void Postfix()
+		{
+			if (!FikaNotificationManager.Exists)
+			{
+				Singleton<PreloaderUI>.Instance.gameObject.AddComponent<FikaNotificationManager>();
+			}
+		}
+	}
 }
