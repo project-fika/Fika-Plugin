e2924fce73f64d6fe6bbd9761ad3c7c6cff685c7
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-01-28 16:03:34 +0100

    Add SerializationManager

---
+++ Fika.Core/Networking/EFTSerializationManager.cs
@@ -0,0 +1,15 @@
+﻿namespace Fika.Core.Networking
+{
+    public static class EFTSerializationManager
+    {
+        public static GClass1207 GetReader(byte[] data)
+        {
+            return GClass1210.Get(data);
+        }
+
+        public static GClass1212 GetWriter()
+        {
+            return GClass1215.Get();
+        }
+    }
+}
