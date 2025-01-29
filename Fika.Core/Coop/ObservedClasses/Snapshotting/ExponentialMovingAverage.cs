4f38f2ee87aa9945b1fbd2be45e469bbb4aeefb9
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-01-29 13:18:29 +0100

    Refactor ExponentialMovingAverage

--- Fika.Core/Coop/ObservedClasses/Snapshotting/ExponentialMovingAverage.cs
+++ Fika.Core/Coop/ObservedClasses/Snapshotting/ExponentialMovingAverage.cs
@@ -4,8 +4,8 @@ namespace Fika.Core.Coop.ObservedClasses.Snapshotting
 {
     public struct ExponentialMovingAverage(int n)
     {
-        readonly double alpha = 2.0 / (n + 1);
-        bool initialized = false;
+        private readonly double alpha = 2.0 / (n + 1);
+        private bool initialized = false;
 
         public double Value = 0;
         public double Variance = 0;
@@ -19,12 +19,11 @@ namespace Fika.Core.Coop.ObservedClasses.Snapshotting
                 Value += alpha * delta;
                 Variance = (1 - alpha) * (Variance + alpha * delta * delta);
                 StandardDeviation = Math.Sqrt(Variance);
+                return;
             }
-            else
-            {
-                Value = newValue;
-                initialized = true;
-            }
+
+            Value = newValue;
+            initialized = true;
         }
 
         public void Reset()
