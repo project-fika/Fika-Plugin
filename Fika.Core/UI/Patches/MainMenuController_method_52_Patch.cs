e174afb3d935361bb7ed05da60a2b27bb17c1c14
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-02-09 09:37:18 +0100

    Update patch

--- Fika.Core/UI/Patches/MainMenuController_method_52_Patch.cs
+++ Fika.Core/UI/Patches/MainMenuController_method_52_Patch.cs
@@ -6,11 +6,11 @@ namespace Fika.Core.UI.Patches
     /// <summary>
     /// This allows all game editions to edit the <see cref="EFT.RaidSettings"/>
     /// </summary>
-    public class MainMenuController_method_49_Patch : ModulePatch
+    public class MainMenuController_method_52_Patch : ModulePatch
     {
         protected override MethodBase GetTargetMethod()
         {
-            return typeof(MainMenuControllerClass).GetMethod(nameof(MainMenuControllerClass.method_49));
+            return typeof(MainMenuControllerClass).GetMethod(nameof(MainMenuControllerClass.method_52));
         }
 
         [PatchPrefix]
