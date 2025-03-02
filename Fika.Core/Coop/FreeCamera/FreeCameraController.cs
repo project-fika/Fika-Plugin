6dd675af9cc464670e3f936f324a06b4088c5862
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-03-02 14:28:44 +0100

    Fix incorrect cast

--- Fika.Core/Coop/FreeCamera/FreeCameraController.cs
+++ Fika.Core/Coop/FreeCamera/FreeCameraController.cs
@@ -317,10 +317,10 @@ namespace Fika.Core.Coop.FreeCamera
                 Destroy(bloodOnScreen);
             }
 
-            List<EffectsController.Class633> effectsManagerList = effectsController.Field<List<EffectsController.Class633>>("list_0").Value;
+            List<EffectsController.Class632> effectsManagerList = effectsController.Field<List<EffectsController.Class632>>("list_0").Value;
             if (effectsManagerList != null)
             {
-                foreach (EffectsController.Class633 effectsManager in effectsManagerList)
+                foreach (EffectsController.Class632 effectsManager in effectsManagerList)
                 {
                     while (effectsManager.ActiveEffects.Count > 0)
                     {
