fcbbf8244db36b68bdc729f72e8f355d2dd3e406
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-03-03 12:59:34 +0100

    Update docs

--- Fika.Core/Coop/Players/ObservedCoopPlayer.cs
+++ Fika.Core/Coop/Players/ObservedCoopPlayer.cs
@@ -33,9 +33,9 @@ using static Fika.Core.UI.FikaUIGlobals;
 namespace Fika.Core.Coop.Players
 {
     /// <summary>
-    /// Observed players are any other players in the world for a client, including bots.
-    /// They are all being handled by the server that moves them through packets.
-    /// As a host this is only other clients.
+    /// Observed players are any other players in the world for a client, including bots. <br/>
+    /// Bots are handled by the server, and other clients send their own data which the server replicates to other clients. <br/>
+    /// As a host all <see cref="ObservedCoopPlayer"/>s are only other clients.
     /// </summary>
     public class ObservedCoopPlayer : CoopPlayer
     {
