5aa4af51aad7e2721b863561394a835739e7584a
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-04-02 10:58:19 +0200

    Cleanup class

--- Fika.Core/Coop/Players/ObservedCoopPlayer.cs
+++ Fika.Core/Coop/Players/ObservedCoopPlayer.cs
@@ -25,7 +25,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.Assertions.Must;
 using UnityEngine.Audio;
 using static Fika.Core.Networking.CommonSubPackets;
 using static Fika.Core.Networking.SubPacket;
