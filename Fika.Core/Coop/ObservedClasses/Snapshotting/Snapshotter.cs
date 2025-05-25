2916ee68e5247a838fd704767710c15fcd2885fc
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-05-25 12:04:02 +0200

    Rename vars and use lock for snapshotting (testing)

--- Fika.Core/Coop/ObservedClasses/Snapshotting/Snapshotter.cs
+++ Fika.Core/Coop/ObservedClasses/Snapshotting/Snapshotter.cs
@@ -8,51 +8,53 @@ namespace Fika.Core.Coop.ObservedClasses.Snapshotting
 {
     public abstract class Snapshotter<T> where T : ISnapshot
     {
-        private readonly SortedList<double, T> buffer;
-        private double localTimeline;
-        private double localTimeScale;
-        private readonly SnapshotInterpolationSettings interpolationSettings;
-        private ExponentialMovingAverage driftEma;
-        private ExponentialMovingAverage deliveryTimeEma;
-        private readonly int sendRate;
-        private readonly float sendInterval;
-        private double bufferTimeMultiplier;
+        private readonly SortedList<double, T> _buffer;
+        private double _localTimeline;
+        private double _localTimeScale;
+        private readonly SnapshotInterpolationSettings _interpolationSettings;
+        private ExponentialMovingAverage _driftEma;
+        private ExponentialMovingAverage _deliveryTimeEma;
+        private readonly int _sendRate;
+        private readonly float _sendInterval;
+        private double _bufferTimeMultiplier;
+        private object _bufferLock;
 
         protected Snapshotter()
         {
-            buffer = new(32);
-            localTimeScale = Time.timeScale;
-            sendRate = Singleton<IFikaNetworkManager>.Instance.SendRate;
-            interpolationSettings = new();
-            bufferTimeMultiplier = interpolationSettings.bufferTimeMultiplier;
-            driftEma = new(sendRate * interpolationSettings.driftEmaDuration);
-            deliveryTimeEma = new(sendRate * interpolationSettings.deliveryTimeEmaDuration);
-            sendInterval = 1f / sendRate;
+            _buffer = new(32);
+            _localTimeScale = Time.timeScale;
+            _sendRate = Singleton<IFikaNetworkManager>.Instance.SendRate;
+            _interpolationSettings = new();
+            _bufferTimeMultiplier = _interpolationSettings.bufferTimeMultiplier;
+            _driftEma = new(_sendRate * _interpolationSettings.driftEmaDuration);
+            _deliveryTimeEma = new(_sendRate * _interpolationSettings.deliveryTimeEmaDuration);
+            _sendInterval = 1f / _sendRate;
+            _bufferLock = new();
         }
 
         private double BufferTime
         {
             get
             {
-                return sendInterval * bufferTimeMultiplier;
+                return _sendInterval * _bufferTimeMultiplier;
             }
         }
 
         /// <summary>
-        /// Checks the <see cref="buffer"/> and <see cref="ObservedCoopPlayer.Interpolate(ref PlayerStatePacket, ref PlayerStatePacket, double)"/>s any snapshots
+        /// Checks the <see cref="_buffer"/> and <see cref="ObservedCoopPlayer.Interpolate(ref PlayerStatePacket, ref PlayerStatePacket, double)"/>s any snapshots
         /// </summary>
         public void ManualUpdate(float unscaledDeltaTime)
         {
-            if (buffer.Count > 0)
+            if (_buffer.Count > 0)
             {
-                SnapshotInterpolation.Step(buffer, unscaledDeltaTime, ref localTimeline, localTimeScale, out T fromSnapshot,
+                SnapshotInterpolation.Step(_buffer, unscaledDeltaTime, ref _localTimeline, _localTimeScale, out T fromSnapshot,
                     out T toSnapshot, out double ratio);
                 Interpolate(toSnapshot, fromSnapshot, (float)ratio);
             }
         }
 
         /// <summary>
-        /// Interpolates states in the <see cref="buffer"/>
+        /// Interpolates states in the <see cref="_buffer"/>
         /// </summary>
         /// <param name="to">Goal state</param>
         /// <param name="from">State to lerp from</param>
@@ -60,33 +62,36 @@ namespace Fika.Core.Coop.ObservedClasses.Snapshotting
         public abstract void Interpolate(in T to, in T from, float ratio);
 
         /// <summary>
-        /// Inserts a snapshot to the <see cref="buffer"/>
+        /// Inserts a snapshot to the <see cref="_buffer"/>
         /// </summary>
         /// <param name="snapshot"></param>
         public void Insert(T snapshot, double networkTime)
         {
             //localTimeline > snapshot.RemoteTime
-            if (buffer.Count > interpolationSettings.bufferLimit)
+            lock (_bufferLock)
             {
-                buffer.Clear();
+                if (_buffer.Count > _interpolationSettings.bufferLimit)
+                {
+                    _buffer.Clear();
+                }
+
+                snapshot.LocalTime = networkTime;
+
+                _bufferTimeMultiplier = SnapshotInterpolation.DynamicAdjustment(_sendInterval,
+                    _deliveryTimeEma.StandardDeviation, _interpolationSettings.dynamicAdjustmentTolerance);
+
+                SnapshotInterpolation.InsertAndAdjust(_buffer, _interpolationSettings.bufferLimit, snapshot, ref _localTimeline, ref _localTimeScale,
+                    _sendInterval, BufferTime, _interpolationSettings.catchupSpeed, _interpolationSettings.slowdownSpeed, ref _driftEma,
+                    _interpolationSettings.catchupNegativeThreshold, _interpolationSettings.catchupPositiveThreshold, ref _deliveryTimeEma); 
             }
-
-            snapshot.LocalTime = networkTime;
-
-            bufferTimeMultiplier = SnapshotInterpolation.DynamicAdjustment(sendInterval,
-                deliveryTimeEma.StandardDeviation, interpolationSettings.dynamicAdjustmentTolerance);
-
-            SnapshotInterpolation.InsertAndAdjust(buffer, interpolationSettings.bufferLimit, snapshot, ref localTimeline, ref localTimeScale,
-                sendInterval, BufferTime, interpolationSettings.catchupSpeed, interpolationSettings.slowdownSpeed, ref driftEma,
-                interpolationSettings.catchupNegativeThreshold, interpolationSettings.catchupPositiveThreshold, ref deliveryTimeEma);
         }
 
         /// <summary>
-        /// Clears the <see cref="buffer"/>
+        /// Clears the <see cref="_buffer"/>
         /// </summary>
         public void Clear()
         {
-            buffer.Clear();
+            _buffer.Clear();
         }
     }
 }
