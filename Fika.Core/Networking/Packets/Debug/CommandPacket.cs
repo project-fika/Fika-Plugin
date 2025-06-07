ed8c2075fd3bf87cdffcf2741b71c8b408c7fb8e
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-06-07 20:28:18 +0200

    Replicate console commands

---
+++ Fika.Core/Networking/Packets/Debug/CommandPacket.cs
@@ -0,0 +1,31 @@
+﻿using LiteNetLib.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fika.Core.Networking.Packets.Debug
+{
+    public class CommandPacket : INetSerializable
+    {
+
+
+        public void Deserialize(NetDataReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Serialize(NetDataWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public enum ECommandType
+        {
+            SpawnAI,
+            ClearAI,
+            Bring
+        }
+    }
+}
