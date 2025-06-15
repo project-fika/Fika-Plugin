405e901b94985deadd8e1a26f089929b8c16f8dd
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-06-15 09:58:43 +0200

    Update interpolator

--- Fika.Core/Coop/ObservedClasses/Snapshotting/ObservedSnapshotter.cs
+++ Fika.Core/Coop/ObservedClasses/Snapshotting/ObservedSnapshotter.cs
@@ -6,29 +6,29 @@ namespace Fika.Core.Coop.ObservedClasses.Snapshotting
 {
     public class ObservedSnapshotter(ObservedCoopPlayer observedPlayer) : Snapshotter<PlayerStatePacket>
     {
-        private readonly ObservedCoopPlayer player = observedPlayer;
+        private readonly ObservedCoopPlayer _player = observedPlayer;
 
         public override void Interpolate(in PlayerStatePacket to, in PlayerStatePacket from, float ratio)
         {
-            player.CurrentPlayerState.Rotation = new Vector2(Mathf.LerpAngle(from.Rotation.x, to.Rotation.x, ratio),
-                Mathf.LerpUnclamped(from.Rotation.y, to.Rotation.y, ratio));
-            player.CurrentPlayerState.HeadRotation = Vector3.LerpUnclamped(from.HeadRotation, to.HeadRotation, ratio);
-            player.CurrentPlayerState.PoseLevel = from.PoseLevel + (to.PoseLevel - from.PoseLevel);
-            player.CurrentPlayerState.Position = Vector3.LerpUnclamped(from.Position, to.Position, ratio);
-            player.CurrentPlayerState.Tilt = Mathf.LerpUnclamped(from.Tilt, to.Tilt, ratio);
-            player.CurrentPlayerState.MovementDirection = Vector2.Lerp(from.MovementDirection, to.MovementDirection, ratio);
-            player.CurrentPlayerState.State = to.State;
-            player.CurrentPlayerState.Tilt = Mathf.LerpUnclamped(from.Tilt, to.Tilt, ratio);
-            player.CurrentPlayerState.Step = to.Step;
-            player.CurrentPlayerState.CharacterMovementSpeed = to.CharacterMovementSpeed;
-            player.CurrentPlayerState.IsProne = to.IsProne;
-            player.CurrentPlayerState.PoseLevel = Mathf.LerpUnclamped(from.PoseLevel, to.PoseLevel, ratio);
-            player.CurrentPlayerState.IsSprinting = to.IsSprinting;
-            player.CurrentPlayerState.Stamina = to.Stamina;
-            player.CurrentPlayerState.Blindfire = to.Blindfire;
-            player.CurrentPlayerState.WeaponOverlap = Mathf.LerpUnclamped(from.WeaponOverlap, to.WeaponOverlap, ratio);
-            player.CurrentPlayerState.LeftStanceDisabled = to.LeftStanceDisabled;
-            player.CurrentPlayerState.IsGrounded = to.IsGrounded;
+            _player.CurrentPlayerState.Rotation = new Vector2(Mathf.LerpAngle(from.Rotation.x, to.Rotation.x, ratio),
+                Mathf.Lerp(from.Rotation.y, to.Rotation.y, ratio));
+            _player.CurrentPlayerState.HeadRotation = Vector3.LerpUnclamped(from.HeadRotation, to.HeadRotation, ratio);
+            _player.CurrentPlayerState.PoseLevel = from.PoseLevel + (to.PoseLevel - from.PoseLevel);
+            _player.CurrentPlayerState.Position = Vector3.LerpUnclamped(from.Position, to.Position, ratio);
+            _player.CurrentPlayerState.Tilt = Mathf.LerpUnclamped(from.Tilt, to.Tilt, ratio);
+            _player.CurrentPlayerState.MovementDirection = Vector2.Lerp(from.MovementDirection, to.MovementDirection, ratio);
+            _player.CurrentPlayerState.State = to.State;
+            _player.CurrentPlayerState.Tilt = Mathf.LerpUnclamped(from.Tilt, to.Tilt, ratio);
+            _player.CurrentPlayerState.Step = to.Step;
+            _player.CurrentPlayerState.CharacterMovementSpeed = to.CharacterMovementSpeed;
+            _player.CurrentPlayerState.IsProne = to.IsProne;
+            _player.CurrentPlayerState.PoseLevel = Mathf.LerpUnclamped(from.PoseLevel, to.PoseLevel, ratio);
+            _player.CurrentPlayerState.IsSprinting = to.IsSprinting;
+            _player.CurrentPlayerState.Stamina = to.Stamina;
+            _player.CurrentPlayerState.Blindfire = to.Blindfire;
+            _player.CurrentPlayerState.WeaponOverlap = Mathf.LerpUnclamped(from.WeaponOverlap, to.WeaponOverlap, ratio);
+            _player.CurrentPlayerState.LeftStanceDisabled = to.LeftStanceDisabled;
+            _player.CurrentPlayerState.IsGrounded = to.IsGrounded;
         }
     }
 }
