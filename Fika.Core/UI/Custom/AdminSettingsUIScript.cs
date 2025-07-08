d6f825eddf94c5d6c5031c36614a30f2cf697b52
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-07-08 14:05:00 +0200

    Update bundles and add new menu

---
+++ Fika.Core/UI/Custom/AdminSettingsUIScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdminSettingsUIScript : MonoBehaviour
+{
+    public Toggle FriendlyFireCheck;
+    public Toggle FreecamCheck;
+    public Toggle SpectateFreecamCheck;
+    public Toggle SharedQuestProgressionCheck;
+    public Toggle AverageLevelCheck;
+
+    public Button ApplyButton;
+    public Button CloseButton;
+}
