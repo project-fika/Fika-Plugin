0b3468d75fc85eeed8ac32d9d0a322bce85ee472
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-07-14 15:34:16 +0200

    Update bundles

---
+++ Fika.Core/UI/Custom/FikaChatUI.cs
@@ -0,0 +1,11 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FikaChatUI : MonoBehaviour
+{
+    public TextMeshProUGUI ChatText;
+    public TextMeshProUGUI MessageText;
+    public ScrollRect ScrollView;
+    public Button SendButton;
+}
