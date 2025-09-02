6e5ea3e2d11f7c407c913c27add2cd140189e691
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-09-02 08:12:30 +0200

    Update bundle and script

--- Fika.Core/UI/Custom/FreecamUI.cs
+++ Fika.Core/UI/Custom/FreecamUI.cs
@@ -3,12 +3,12 @@
 public class FreecamUI : MonoBehaviour
 {
     public TextMeshProUGUI JumpText;
-    public TextMeshProUGUI Jump3rdText;
-    public TextMeshProUGUI JumpHeadText;
+    public TextMeshProUGUI SwapMode;
     public TextMeshProUGUI DetachText;
     public TextMeshProUGUI TeleportText;
     public TextMeshProUGUI VisionText;
     public TextMeshProUGUI CullingText;
     public TextMeshProUGUI InputText;
     public TextMeshProUGUI TurboText;
+    public TextMeshProUGUI PlayerList;
 }
