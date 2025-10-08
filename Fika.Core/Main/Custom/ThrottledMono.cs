47a9d37aa40e2e7cc0b9628c7114115cd3805cd4
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-10-08 10:50:48 +0200

    Add arkup

--- Fika.Core/Main/Custom/ThrottledMono.cs
+++ Fika.Core/Main/Custom/ThrottledMono.cs
@@ -1,20 +1,37 @@
 ﻿namespace Fika.Core.Main.Custom;
 
+/// <summary>
+/// An abstract MonoBehaviour that throttles the frequency of <see cref="Tick"/> calls
+/// based on a specified <see cref="UpdateRate"/>.
+/// </summary>
 public abstract class ThrottledMono : MonoBehaviour
 {
     /// <summary>
-    /// Number of ticks per second (e.g., 10 means <see cref="Tick"/> runs 10 times per second).
+    /// Gets the number of ticks per second. <br/>
+    /// E.g., a value of 10 means <see cref="Tick"/> is called 10 times per second.
     /// </summary>
     public abstract float UpdateRate { get; }
 
     private float _secondsPerTick;
     private float _tickTimer;
 
-    private void Awake()
+    /// <summary>
+    /// Called when the script instance is being loaded.
+    /// Initializes the tick interval based on <see cref="UpdateRate"/>. <br/>
+    /// If you override this, ensure you call the base implementation or <see cref="ThrottledMono"/> will not function correctly.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="_secondsPerTick"/> is assigned during <see cref="Awake"/>.
+    /// </remarks>
+    protected virtual void Awake()
     {
         _secondsPerTick = 1f / UpdateRate;
     }
 
+    /// <summary>
+    /// Unity callback called every frame.
+    /// Accumulates unscaled delta time and calls <see cref="Tick"/> when the interval elapses.
+    /// </summary>
     private void Update()
     {
         _tickTimer += Time.unscaledDeltaTime;
@@ -25,5 +42,9 @@ public abstract class ThrottledMono : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Called at the throttled interval defined by <see cref="UpdateRate"/>.
+    /// Implement this method to define custom tick behavior.
+    /// </summary>
     public abstract void Tick();
 }
