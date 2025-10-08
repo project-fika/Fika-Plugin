60d9249b25f84fcd4407008bc4b1644304a07956
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-10-08 10:46:22 +0200

    Add ThrottledMono

---
+++ Fika.Core/Main/Custom/ThrottledMono.cs
@@ -0,0 +1,29 @@
+﻿namespace Fika.Core.Main.Custom;
+
+public abstract class ThrottledMono : MonoBehaviour
+{
+    /// <summary>
+    /// Number of ticks per second (e.g., 10 means <see cref="Tick"/> runs 10 times per second).
+    /// </summary>
+    public abstract float UpdateRate { get; }
+
+    private float _secondsPerTick;
+    private float _tickTimer;
+
+    private void Awake()
+    {
+        _secondsPerTick = 1f / UpdateRate;
+    }
+
+    private void Update()
+    {
+        _tickTimer += Time.unscaledDeltaTime;
+        if (_tickTimer >= _secondsPerTick)
+        {
+            Tick();
+            _tickTimer -= _secondsPerTick; // monos in Tarkov don't last long enough for accumulation drift to occur
+        }
+    }
+
+    public abstract void Tick();
+}
