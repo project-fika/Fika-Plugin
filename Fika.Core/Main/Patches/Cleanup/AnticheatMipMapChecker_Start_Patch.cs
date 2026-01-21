a9c0ce06709d9d6188a46681f9ccdc9fba2b2dab
Lacyway <20912169+Lacyway@users.noreply.github.com>
2026-01-21 09:26:35 +0100

    Add cleanup patch

---
+++ Fika.Core/Main/Patches/Cleanup/AnticheatMipMapChecker_Start_Patch.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+
+namespace Fika.Core.Main.Patches.Cleanup;
+
+internal class AnticheatMipMapChecker_Start_Patch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return AccessTools.Method(typeof(AnticheatMipMapChecker),
+            nameof(AnticheatMipMapChecker.Start));
+    }
+
+    [PatchPrefix]
+    public static bool Prefix(AnticheatMipMapChecker __instance)
+    {
+        GameObject.Destroy(__instance.gameObject);
+        return false;
+    }
+}
