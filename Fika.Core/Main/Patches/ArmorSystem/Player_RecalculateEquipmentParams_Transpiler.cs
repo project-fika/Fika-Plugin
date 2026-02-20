42e445169ef2cf0733791f93ea08ac533eba97b6
Lacyway <20912169+Lacyway@users.noreply.github.com>
2026-02-20 15:28:35 +0100

    Fix armor patch

--- Fika.Core/Main/Patches/ArmorSystem/Player_RecalculateEquipmentParams_Transpiler.cs
+++ Fika.Core/Main/Patches/ArmorSystem/Player_RecalculateEquipmentParams_Transpiler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using EFT;
 using HarmonyLib;
 using SPT.Reflection.Patching;
@@ -22,7 +23,28 @@ internal class Player_RecalculateEquipmentParams_Transpiler : ModulePatch
     public static IEnumerable<CodeInstruction> Transpile(IEnumerable<CodeInstruction> instructions)
     {
         var codes = instructions.ToList();
+
+        MigrateLabels(codes, 148, 3);
+        codes.RemoveRange(148, 3);
+
+        MigrateLabels(codes, 7, 8);
         codes.RemoveRange(7, 8);
         return codes;
+    }
+
+    private static void MigrateLabels(List<CodeInstruction> codes, int index, int count)
+    {
+        var targetIndex = index + count;
+
+        if (targetIndex < codes.Count)
+        {
+            var labelsToMove = new List<Label>();
+            for (var i = index; i < targetIndex; i++)
+            {
+                labelsToMove.AddRange(codes[i].labels);
+            }
+
+            codes[targetIndex].labels.AddRange(labelsToMove);
+        }
     }
 }
