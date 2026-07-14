c29f091f43fcd484a0ed1468759676a396c60ae9
Lacyway <20912169+Lacyway@users.noreply.github.com>
2026-07-14 09:40:56 +0200

    Change packet type to MongoID

--- Fika.Core/Networking/Packets/Communication/QuestConditionPacket.cs
+++ Fika.Core/Networking/Packets/Communication/QuestConditionPacket.cs
@@ -1,22 +1,24 @@
-﻿namespace Fika.Core.Networking.Packets.Communication;
+﻿using EFT;
+
+namespace Fika.Core.Networking.Packets.Communication;
 
 public struct QuestConditionPacket : INetSerializable
 {
     public string Nickname;
-    public string Id;
+    public MongoID Id;
     public string SourceId;
 
     public void Deserialize(NetDataReader reader)
     {
         Nickname = reader.GetString();
-        Id = reader.GetString();
+        Id = reader.GetMongoID();
         SourceId = reader.GetString();
     }
 
     public readonly void Serialize(NetDataWriter writer)
     {
         writer.Put(Nickname);
-        writer.Put(Id);
+        writer.PutMongoID(Id);
         writer.Put(SourceId);
     }
 }
