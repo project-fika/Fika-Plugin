d426c57300ed66f0cdd7ab50f4514476ebebf00e
Lacyway <20912169+Lacyway@users.noreply.github.com>
2026-07-14 12:41:38 +0200

    Update switch statement

--- Fika.Core/Networking/Packets/Backend/InraidQuestPacket.cs
+++ Fika.Core/Networking/Packets/Backend/InraidQuestPacket.cs
@@ -40,8 +40,6 @@ public struct InRaidQuestPacket : INetSerializable
                     }
                 }
                 break;
-            default:
-                break;
         }
     }
 
