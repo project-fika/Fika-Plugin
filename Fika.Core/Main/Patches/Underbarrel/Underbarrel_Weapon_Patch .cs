fd03758638b95503bece29dd189d4dc04b26f162
Lacyway <20912169+Lacyway@users.noreply.github.com>
2026-09-19 19:28:10 +0200

    Add backported SPT memory patch

---
+++ Fika.Core/Main/Patches/Underbarrel/Underbarrel_Weapon_Patch .cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using EFT.InventoryLogic;
+using SPT.Reflection.Patching;
+
+namespace Fika.Core.Main.Patches.Underbarrel;
+
+/// <summary>
+/// Backported from SPT 5.0 <br/>
+/// Created by Archangel
+/// </summary>
+public sealed class Underbarrel_Weapon_Patch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(Weapon)
+            .GetMethod(nameof(Weapon.GetUnderbarrelWeapon));
+    }
+
+    [PatchPrefix]
+    public static bool PatchPrefix(Weapon __instance, ref Launcher __result)
+    {
+        __result = GetUnderbarrelWeapon(__instance);
+        return false;
+    }
+
+    private static Launcher GetUnderbarrelWeapon(CompoundItem item)
+    {
+        var slots = item?.Slots;
+        if (slots == null)
+        {
+            return null;
+        }
+
+        for (var i = 0; i < slots.Length; i++)
+        {
+            var slot = slots[i];
+            if (slot != null && slot.ContainedItem is Launcher launcher)
+            {
+                return launcher;
+            }
+        }
+
+        for (var i = 0; i < slots.Length; i++)
+        {
+            var slot = slots[i];
+            if (slot != null && slot.ContainedItem is CompoundItem childCompound)
+            {
+                var found = GetUnderbarrelWeapon(childCompound);
+                if (found != null)
+                {
+                    return found;
+                }
+            }
+        }
+
+        return null;
+    }
+}
